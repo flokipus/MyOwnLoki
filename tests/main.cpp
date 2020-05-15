ecee1f0b10d67cfd044d94c510432891a34c5241
flokipus <al.petrov239@gmail.com>
2020-05-15 19:05:05 +0300

    added arithmetical mean algorithm

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,9 +2,22 @@
 // Created by elrond on 11/1/19.
 //
 
-#include "../singleton/inc/Singleton.h"
+#include <mean.h>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <limits>
 
 int main()
 {
+    std::vector<int> v = {1,2,3,4,5};
+    auto sec = v.begin();
+    sec++;
 
+    double value = lokimath::mean<double>::calc(v.begin(), v.end());
+    double exact = 3.0;
+    double diff = abs(value - exact);
+    bool test_is_fine = (diff == 0.0);
+    std::cout << "lokimath::mean is fine: " << test_is_fine << std::endl;
 }
